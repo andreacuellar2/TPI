46bf823aa333a8347bf1f5f622bc0977748ca860
Andrea Cuellar <andreacuellar@github.com>
2018-04-06 00:40:31 -0600

    Rule y pruebas unidad

--- src/test/java/sv/edu/ues/fmocc/ingenieria/tpi135/guia02/guia02/TipoResponsableFacadeRefactoryTest.java
+++ src/test/java/sv/edu/ues/fmocc/ingenieria/tpi135/guia02/guia02/TipoResponsableFacadeRefactoryTest.java
@@ -8,7 +8,9 @@ import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.powermock.reflect.Whitebox;
+import static sv.edu.ues.fmocc.ingenieria.tpi135.guia02.guia02.EntityManagerProvider.persistenceUnit;
 import sv.edu.ues.fmocc.ingenieria.tpi135.mantenimiento.mantenimientolib.entity.TipoResponsable;
 
 /**
@@ -16,21 +18,17 @@ import sv.edu.ues.fmocc.ingenieria.tpi135.mantenimiento.mantenimientolib.entity.
  * @author ale
  */
 public class TipoResponsableFacadeRefactoryTest {
-
-    protected static EntityManagerFactory emf;
-    protected static EntityManager em;
-
+    
+    @Rule
+    public EntityManagerProvider emProvider = persistenceUnit("mantenimientoTestPU");
+    
     @BeforeClass
     public static void init() {
-        emf = Persistence.createEntityManagerFactory("mantenimientoTestPU");
-        em = emf.createEntityManager();
-
     }
-
     @Test
     public void when_creating_null_tipo_responsable_expect_false() {
         TipoResponsableFacade trf = new TipoResponsableFacade();
-        Whitebox.setInternalState(trf, "em", em);
+        Whitebox.setInternalState(trf, "em", emProvider.em());
         trf.getEntityManager().getTransaction().begin();
 
         boolean result = trf.crear(null);
@@ -47,7 +45,7 @@ public class TipoResponsableFacadeRefactoryTest {
         nuevoTipoResponsable.setActivo(true);
 
         TipoResponsableFacade trf = new TipoResponsableFacade();
-        Whitebox.setInternalState(trf, "em", em);
+        Whitebox.setInternalState(trf, "em", emProvider.em());
 
         trf.getEntityManager().getTransaction().begin();
        
@@ -60,7 +58,7 @@ public class TipoResponsableFacadeRefactoryTest {
     @Test
     public void when_modify_valid_tipo_responsable_expect_true() {
         TipoResponsableFacade trf = new TipoResponsableFacade();
-        Whitebox.setInternalState(trf, "em", em);
+        Whitebox.setInternalState(trf, "em", emProvider.em());
         trf.getEntityManager().getTransaction().begin();
         trf.getEntityManager().persist(new TipoResponsable(null, "test tipo responsable", true));
 
@@ -76,7 +74,7 @@ public class TipoResponsableFacadeRefactoryTest {
     @Test
     public void when_delete_null_tipo_responsable_expect_false() {
         TipoResponsableFacade trf = new TipoResponsableFacade();
-        Whitebox.setInternalState(trf, "em", em);
+        Whitebox.setInternalState(trf, "em", emProvider.em());
         trf.getEntityManager().getTransaction().begin();
         boolean result = trf.eliminar(null);
         assertEquals(0, trf.findAll().size());
@@ -86,24 +84,85 @@ public class TipoResponsableFacadeRefactoryTest {
     @Test
     public void when_delete_valid_tipo_responsable_expect_true() {
         TipoResponsableFacade trf = new TipoResponsableFacade();
-        Whitebox.setInternalState(trf, "em", em);
+        Whitebox.setInternalState(trf, "em", emProvider.em());
         trf.getEntityManager().getTransaction().begin();
-        trf.getEntityManager().persist(new TipoResponsable(null, "test tipo responsable", true));
-        TipoResponsable entity = new TipoResponsable(1);
-        boolean result = trf.eliminar(entity);
-        assertTrue(result);
+       trf.getEntityManager().persist(new TipoResponsable(null, "test tipo responsable", true));
+       TipoResponsable entity = new TipoResponsable(1);
+       boolean result = trf.eliminar(entity);
+       assertTrue(result);
+   }
+    @Test
+    public void testFind_existing_entity() throws Exception {
+        System.out.println("find existing id");
+        Object id = 1;
+        TipoResponsableFacade trf = new TipoResponsableFacade();
+        Whitebox.setInternalState(trf, "em", emProvider.em());
+        trf.getEntityManager().getTransaction().begin();
+        trf.getEntityManager().persist(new TipoResponsable(1, "test tipo responsable", true));
+        int expResult = 1;
+        TipoResponsable result = trf.find(id);
+        assertEquals(expResult, Integer.parseInt(String.valueOf(result.getIdTipoResponsable())));
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    /**
+     * Test of find method, when the searched entity does not exist, of class TipoResponsableFacade.
+     */
+    @Test
+    public void testFind_non_existing_entity() throws Exception {
+        System.out.println("find non existing id");
+        Object id = 1;
+        TipoResponsableFacade trf = new TipoResponsableFacade();
+        Whitebox.setInternalState(trf, "em", emProvider.em());
+        trf.getEntityManager().getTransaction().begin();
+        TipoResponsable result = trf.find(id);
+        assertEquals(null, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
     }
 
+    /**
+     * Test of findAll method, when entity is not empty, of class TipoResponsableFacade.
+     */
+    
+    @Test
+    public void testFindAll() throws Exception {
+        System.out.println("findAll not empty");
+        TipoResponsableFacade trf = new TipoResponsableFacade();
+        Whitebox.setInternalState(trf, "em", emProvider.em());
+        trf.getEntityManager().getTransaction().begin();
+        trf.getEntityManager().persist(new TipoResponsable(null, "test tipo responsable1", true));
+        List<TipoResponsable> result = trf.findAll();
+        assertEquals(1, result.size());
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    /**
+     * Test of count method, of class TipoResponsableFacade.
+     */
+    
+    @Test
+    public void testCount() throws Exception {
+        System.out.println("count");
+        TipoResponsableFacade trf = new TipoResponsableFacade();
+        Whitebox.setInternalState(trf, "em", emProvider.em());
+        trf.getEntityManager().getTransaction().begin();
+        trf.getEntityManager().persist(new TipoResponsable(null, "test tipo responsable1", true));
+        int expResult = 1;
+        int result = trf.count();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+   }
+    
+    
     @After
     public void cleanup() {
-        em.getTransaction().rollback();
+        emProvider.em().getTransaction().rollback();
     }
 
     @AfterClass
     public static void tearDown() {
-        em.clear();
-        em.close();
-        emf.close();
+        
     }
 
 }
